ac0d5a648c793377636184fe29096ac48f8150de
Job <job@vv32.nl>
2021-01-03 00:34:47 +0100

    Fixed marked tiles not disappearing
    Also simplified code

--- BogPlugin.java
+++ BogPlugin.java
@@ -1,32 +1,8 @@
 /*
- * Copyright (c) 2018, Jeremy Berchtold <https://github.com/i-yam-jeremy>
- * All rights reserved.
+ * Copyright (c) 2018
+ * Jeremy Berchtold <https://github.com/i-yam-jeremy>
+ * Job Feikens <job@vv32.nl>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
-// Based off RuneLite's Agility Plugin
-/*
- * Copyright (c) 2018, Adam <Adam@sigterm.info>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,36 +27,20 @@
  */
 package net.runelite.client.plugins.bog;
 
-import com.google.inject.Provides;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.inject.Inject;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
-import net.runelite.api.Tile;
+import net.runelite.api.GameState;
+import net.runelite.api.GroundObject;
 import net.runelite.api.TileObject;
-import net.runelite.api.events.DecorativeObjectChanged;
-import net.runelite.api.events.DecorativeObjectDespawned;
-import net.runelite.api.events.DecorativeObjectSpawned;
-import net.runelite.api.events.GameObjectChanged;
-import net.runelite.api.events.GameObjectDespawned;
-import net.runelite.api.events.GameObjectSpawned;
-import net.runelite.api.events.GroundObjectChanged;
-import net.runelite.api.events.GroundObjectDespawned;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GroundObjectSpawned;
-import net.runelite.api.events.ItemDespawned;
-import net.runelite.api.events.ItemSpawned;
-import net.runelite.api.events.WallObjectChanged;
-import net.runelite.api.events.WallObjectDespawned;
-import net.runelite.api.events.WallObjectSpawned;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
 
 @PluginDescriptor(
 	name = "Temple Trekking Bog",
@@ -93,7 +53,7 @@ public class BogPlugin extends Plugin
 	private static final int WALKABLE_BOG_TILE_ID = 13838;
 
 	@Getter
-	private final List<TileObject> walkableBogTiles = new ArrayList<>();
+	final Set<TileObject> walkableBogTiles = new HashSet<>();
 
 	@Inject
 	private OverlayManager overlayManager;
@@ -101,98 +61,35 @@ public class BogPlugin extends Plugin
 	@Inject
 	private BogOverlay bogOverlay;
 
-	@Inject
-	private Client client;
-
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
 		overlayManager.add(bogOverlay);
 	}
 
 	@Override
-	protected void shutDown() throws Exception
+	protected void shutDown()
 	{
 		overlayManager.remove(bogOverlay);
 	}
 
 	@Subscribe
-	public void onGameObjectSpawned(GameObjectSpawned event)
+	public void onGroundObjectSpawned(final GroundObjectSpawned event)
 	{
-		onTileObject(event.getTile(), null, event.getGameObject());
+		final GroundObject groundObject = event.getGroundObject();
+
+		if (groundObject.getId() == WALKABLE_BOG_TILE_ID)
+		{
+			walkableBogTiles.add(groundObject);
+		}
 	}
 
 	@Subscribe
-	public void onGameObjectChanged(GameObjectChanged event)
+	public void onGameStateChanged(final GameStateChanged event)
 	{
-		onTileObject(event.getTile(), event.getPrevious(), event.getGameObject());
-	}
-
-	@Subscribe
-	public void onGameObjectDespawned(GameObjectDespawned event)
-	{
-		onTileObject(event.getTile(), event.getGameObject(), null);
-	}
-
-	@Subscribe
-	public void onGroundObjectSpawned(GroundObjectSpawned event)
-	{
-		onTileObject(event.getTile(), null, event.getGroundObject());
-	}
-
-	@Subscribe
-	public void onGroundObjectChanged(GroundObjectChanged event)
-	{
-		onTileObject(event.getTile(), event.getPrevious(), event.getGroundObject());
-	}
-
-	@Subscribe
-	public void onGroundObjectDespawned(GroundObjectDespawned event)
-	{
-		onTileObject(event.getTile(), event.getGroundObject(), null);
-	}
-
-	@Subscribe
-	public void onWallObjectSpawned(WallObjectSpawned event)
-	{
-		onTileObject(event.getTile(), null, event.getWallObject());
-	}
-
-	@Subscribe
-	public void onWallObjectChanged(WallObjectChanged event)
-	{
-		onTileObject(event.getTile(), event.getPrevious(), event.getWallObject());
-	}
-
-	@Subscribe
-	public void onWallObjectDespawned(WallObjectDespawned event)
-	{
-		onTileObject(event.getTile(), event.getWallObject(), null);
-	}
-
-	@Subscribe
-	public void onDecorativeObjectSpawned(DecorativeObjectSpawned event)
-	{
-		onTileObject(event.getTile(), null, event.getDecorativeObject());
-	}
-
-	@Subscribe
-	public void onDecorativeObjectChanged(DecorativeObjectChanged event)
-	{
-		onTileObject(event.getTile(), event.getPrevious(), event.getDecorativeObject());
-	}
-
-	@Subscribe
-	public void onDecorativeObjectDespawned(DecorativeObjectDespawned event)
-	{
-		onTileObject(event.getTile(), event.getDecorativeObject(), null);
-	}
-
-	private void onTileObject(Tile tile, TileObject oldObject, TileObject newObject)
-	{
-		walkableBogTiles.remove(oldObject);
-		if (newObject != null && newObject.getId() == WALKABLE_BOG_TILE_ID) {
-			walkableBogTiles.add(newObject);
+		if (event.getGameState() == GameState.LOADING)
+		{
+			walkableBogTiles.clear();
 		}
 	}
 }
